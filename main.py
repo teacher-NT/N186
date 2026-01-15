3f7133d1b238733de2a5d98713a827f7d6635013
Mirjamol Mirislomov <alpholmon@gmail.com>
2026-01-15 09:19:41 +0500

    Math

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Salom Github")
+import os
+os.system("cls")
+
+import math
+
+print(math.pow(2, 3))
+
+print(math.sqrt(121))
+
+print(math.log2(128))
+print(math.log(625, 5))
+
+print(math.floor(3.8))
+print(math.ceil(3.45))
+print(round(3.5))
