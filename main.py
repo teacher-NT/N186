196969c69690cd9ef56620c16ef3f4592061b85c
Mirjamol Mirislomov <alpholmon@gmail.com>
2026-01-31 09:01:32 +0500

    Magic methods

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import os
 os.system("cls")
-import json
 
-# car = {
-#     "brand": "GM",
-#     "model": "Damas",
-#     "probeg": 2500,
-#     "rang": "Oq",
-#     "narx": 6500
-# }
+class Car:
+    def __init__(self, b,m,r,n,y):
+        self.brand = b
+        self.model = m
+        self.rang = r
+        self.narx = n
+        self.yil = y
 
-# with open("myfile.txt", "w") as f:
-#     # f.write(car)
-#     json.dump(car, f, indent=4)
+    def __str__(self):
+        return f"{self.brand} {self.model} {self.rang} {self.narx}"
+    
+    def __eq__(self, n):
+        return self.narx == n
 
-with open("myfile.txt") as f:
-    car = json.load(f)
-    print(car)
+    def __ne__(self, n):
+        return self.narx != n
+    
+    def __lt__(self, n):
+        return self.yil < n
+
+car1 = Car("GM", "Matiz", "OQ", 3000, 2022)
+print(car1 == 2000)
+print(car1 != 2000)
+print(car1 < 2020)
+
+print(car1)        
