af891201416c3b9edc9fd605587ea8af348336fb
Mirjamol Mirislomov <alpholmon@gmail.com>
2026-01-31 09:14:23 +0500

    Arifmetik metodlar

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import os
 os.system("cls")
 
 class Car:
-    def __init__(self, b,m,r,n,y):
+    def __init__(self, b,m,r,n,y,s):
         self.brand = b
         self.model = m
         self.rang = r
         self.narx = n
         self.yil = y
+        self.soni = s
 
     def __str__(self):
-        return f"{self.brand} {self.model} {self.rang} {self.narx}"
+        return f"{self.brand} {self.model} {self.rang} {self.narx}. {self.soni}ta"
     
     def __eq__(self, n):
         return self.narx == n
@@ -20,10 +21,22 @@ class Car:
     
     def __lt__(self, n):
         return self.yil < n
+    
+    def __add__(self, n):
+        self.narx += n
 
-car1 = Car("GM", "Matiz", "OQ", 3000, 2022)
-print(car1 == 2000)
-print(car1 != 2000)
-print(car1 < 2020)
+    def __sub__(self, n):
+        self.narx -= n
+    
+    def __mul__(self, n):
+        self.soni *= n
+    
+    def __truediv__(self, n):
+        self.soni //= n
+
+car1 = Car("GM", "Matiz", "OQ", 3000, 2022, 12)
+
+# car1 * 2
+car1 / 3
 
 print(car1)        
