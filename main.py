1860eb1775b0c54f7640ca72085ed05ec2d76763
Mirjamol Mirislomov <alpholmon@gmail.com>
2026-02-05 09:43:55 +0500

    Interfeys 1

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
 import os
 os.system("cls")
+import random as rd
+from PyQt5.QtWidgets import ( 
+    QApplication, QWidget, QLabel,
+    QPushButton
+)
 
-from abc import ABC, abstractmethod
+btn_style = """
+    background-color:#e8e689;
+    color:black;
+    font-size:20px;
+    border-radius:20px;
+    border: 2px solid red;
+"""
 
-class Player(ABC):
-    @abstractmethod
-    def run(self):
-        pass
+app = QApplication([])
 
-    @abstractmethod
-    def jump(self):
-        pass
+oyna = QWidget()
+oyna.setWindowTitle("Mening birinchi dasturim")
+oyna.setGeometry(1350,200, 400,700)
+oyna.setStyleSheet("background-color:#303670;")
 
-    @abstractmethod
-    def kick(self):
-        pass
+label1 = QLabel(oyna)
+label1.setText("Bizning dasturdan salom!")
+label1.setStyleSheet("font-size:30px; color:#ffffff;")
 
+def func_btn1():
+    label1.setText("Ilova tugmasi bosildi...")
+    colors = ["red", "green", "blue", "yellow", "white", "purple"]
+    color = rd.choice(colors)
+    oyna.setStyleSheet(f"background-color:{color};")
+btn1 = QPushButton(oyna)
+btn1.setText("Tugmani bosing")
+btn1.setStyleSheet(btn_style)
+btn1.setGeometry(115,325, 170, 50)
+btn1.clicked.connect(func_btn1)
 
-class General(Player):
-    def __init__(self, n,s):
-        self.nickname = n
-        self.s = s
-    def run(self):
-        print(f"{self.nickname} is running...")
-
-    def kick(self):
-        print(f"{self.nickname} is kicking...")
-
-    def jump(self):
-        print(f"{self.nickname} is jumping...")
-        
-general1 = General("Spiderman", 1000)
+oyna.show()
+app.exec_()
