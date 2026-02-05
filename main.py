4ede1428f821bc61ae59ccbd33b5dbcf337abc4b
Mirjamol Mirislomov <alpholmon@gmail.com>
2026-02-05 10:03:34 +0500

    Interfeys 1

--- main.py
+++ main.py
@@ -14,6 +14,19 @@ btn_style = """
     border: 2px solid red;
 """
 
+reset_style = """
+    background-color:red;
+    color:black;
+    font-size:20px;
+    border-radius:20px;
+    border: 2px solid blue;
+"""
+
+count_style = """
+    font-size:60px;
+    color: black;
+"""
+
 app = QApplication([])
 
 oyna = QWidget()
@@ -22,19 +35,41 @@ oyna.setGeometry(1350,200, 400,700)
 oyna.setStyleSheet("background-color:#303670;")
 
 label1 = QLabel(oyna)
-label1.setText("Bizning dasturdan salom!")
+label1.setText("Qani boshladik")
+label1.move(100, 20)
 label1.setStyleSheet("font-size:30px; color:#ffffff;")
 
+count_label = QLabel(oyna)
+count_label.setText("0")
+count_label.move(180,100)
+count_label.setFixedWidth(100)
+count_label.setStyleSheet(count_style)
+
+count = 0
 def func_btn1():
-    label1.setText("Ilova tugmasi bosildi...")
-    colors = ["red", "green", "blue", "yellow", "white", "purple"]
-    color = rd.choice(colors)
-    oyna.setStyleSheet(f"background-color:{color};")
+    global count
+    count += 1
+    # colors = ["red", "green", "blue", "yellow", "white", "purple"]
+    # color = rd.choice(colors)
+    # oyna.setStyleSheet(f"background-color:{color};")
+    count_label.setText(str(count))
+
 btn1 = QPushButton(oyna)
-btn1.setText("Tugmani bosing")
+btn1.setText("Count")
 btn1.setStyleSheet(btn_style)
 btn1.setGeometry(115,325, 170, 50)
 btn1.clicked.connect(func_btn1)
+
+def reset_func():
+    global count
+    count = 0
+    count_label.setText(str(count))
+
+reset_btn = QPushButton(oyna)
+reset_btn.setText("Reset")
+reset_btn.setStyleSheet(reset_style)
+reset_btn.setGeometry(115,380, 170, 50)
+reset_btn.clicked.connect(reset_func)
 
 oyna.show()
 app.exec_()
