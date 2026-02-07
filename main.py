81663b9316dbd353fb8da841f1123dbc391edc25
Mirjamol Mirislomov <alpholmon@gmail.com>
2026-02-07 09:40:57 +0500

    Layout

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ os.system("cls")
 import random as rd
 from PyQt5.QtWidgets import ( 
     QApplication, QWidget, QLabel,
-    QPushButton, QLineEdit,
+    QPushButton, QLineEdit,QComboBox,
     QVBoxLayout, QHBoxLayout
 )
 
@@ -21,41 +21,63 @@ input_style = """
     border: 3px solid black;
 """
 
+label1_style = """
+    font-size: 40px;
+    color: white;
+    font-family: Impact;
+"""
+
+combo_style = """
+    font-size: 20px;
+    background-color: white;
+    color:black;
+"""
+label2_style = """
+    font-size: 20px;
+    color: white;
+"""
+
 class Window(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("My App")
-        self.setGeometry(1300,200,400,700)
+        # self.setGeometry(1300,200,400,700)
         self.setStyleSheet("background-color:#303670;")
-        self.vbox = QHBoxLayout()
-        
+        self.vbox = QVBoxLayout()
+
+        self.label1 = QLabel("Restoran Menyusi")
+        self.label1.setStyleSheet(label1_style)
+        self.vbox.addWidget(self.label1)
+
+        self.set_combo()
+        self.label2 = QLabel("Tanlangan ovqat: ")
+        self.label2.setStyleSheet(label2_style)
+        self.vbox.addWidget(self.label2)
 
         self.btn1 = QPushButton("Tugma 1")
         self.btn1.setStyleSheet(btn_style)
         self.vbox.addWidget(self.btn1)
         self.vbox.addSpacing(30)
 
-
-        self.btn2 = QPushButton("Tugma 2")
-        self.btn2.setStyleSheet(btn_style)
-        self.vbox.addWidget(self.btn2)
-        self.vbox.addSpacing(30)
-
-        self.btn3 = QPushButton("Tugma 3")
-        self.btn3.setStyleSheet(btn_style)
-        self.vbox.addWidget(self.btn3)
-        self.vbox.addSpacing(30)
-
-        self.btn4 = QPushButton("Tugma 4")
-        self.btn4.setStyleSheet(btn_style)
-        self.vbox.addWidget(self.btn4)
-        self.vbox.addSpacing(30)
-
-        self.btn5 = QPushButton("Tugma 5")
-        self.btn5.setStyleSheet(btn_style)
-        self.vbox.addWidget(self.btn5)
-        
         self.setLayout(self.vbox)
+    
+    def set_combo(self):
+        self.combo = QComboBox()
+        # self.combo.addItem("Osh")
+        # self.combo.addItem("Shashlik")
+        # self.combo.addItem("Mastava")
+        # self.combo.addItem("Sho'rva")
+        # self.combo.addItem("Lag'mon")
+
+        self.combo.addItems(["Osh", "Shashlik", "Mastava","Sho'rva", "Lag'mon"])
+
+        self.combo.setStyleSheet(combo_style)
+        self.combo.currentTextChanged.connect(self.combo_changed)
+
+        self.vbox.addWidget(self.combo)
+    def combo_changed(self):
+        text = self.combo.currentText()
+        self.label2.setText(f"Tanlangan ovqat: {text}")
 
         
 
